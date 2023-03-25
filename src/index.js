6659b613c696c24074198a889c2c9f89e7b73042
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-03-25 20:04:22 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form=document.querySelector('form')
+  
+  form.addEventListener('submit', (e)=>{
+  e.preventDefault()
+  addList(e.target.taskdescription.value)})
+  
 });
+function addList(list){
+  let p= document.createElement('p')
+  p.textContent=list
+  document.querySelector('#list').appendChild(p)
+  console.log(p)
+}
